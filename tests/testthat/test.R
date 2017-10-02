be8bf6e8444f05bc21dbfe45f8394dc7b5eec6b8
ramonlaborda <laborda.ramon@gmail.com>
2017-10-02 21:02:44 +0200

    some

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -20,14 +20,14 @@ test_that("geocodification(address) works",{
                           stringsAsFactors = FALSE),
                tolerance=0.00001)
   #coordinates
-  expect_equal(geocodification("40.41678, -3.70379"),
-               data.frame(address ="Plaza Puerta del Sol, 11, 28013 Madrid, Spain",
-                          latitude=40.41678,
-                          longitude = -3.70379,
+  expect_equal(geocodification("58.41081, 15.62137"),
+               data.frame(address ="Storgatan 48, 582 23 Linköping, Sweden",
+                          latitude=58.41081,
+                          longitude = 15.62137,
                           stringsAsFactors = FALSE),
                tolerance=0.00001)
   #address ERROR
-  expect_equal(geocodification("12.5asadsd"),
+  expect_equal(geocodification("qwertyuiop1234"),
                data.frame(address ="ERROR",
                           latitude=0,
                           longitude = 0,
