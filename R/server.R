c042ccbd3f06c8be7e7d36da07bdb77f6310ccc9
ramonlaborda <laborda.ramon@gmail.com>
2017-10-03 17:47:35 +0200

    shiny

--- R/server.R
+++ R/server.R
@@ -3,7 +3,8 @@ library(lab5)
 
 shinyServer(
   function(input,output){
-    output$ads <- renderPrint(geocodification(input$ddss))
-
+    output$ads <- renderPrint(geocodification(input$ddss)[1])
+    output$lat <- renderPrint(geocodification(input$ddss)[2])
+    output$lon <- renderPrint(geocodification(input$ddss)[3])
   }
 )
--- R/ui.R
+++ R/ui.R
@@ -7,8 +7,8 @@ shinyUI(fluidPage(
   titlePanel(title = "Geocodification"),
   sidebarLayout(
     sidebarPanel(("Type an address or a pair of numbers (coordinates)"),
-      textInput("ddss","Here: ",""),textOutput("ads")),
-    mainPanel("Map")
+      textInput("ddss","Here: ",""),textOutput("ads"),textOutput("lat"),textOutput("lon")),
+    mainPanel("")
   )
  )
 )
